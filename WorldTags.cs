adc28b71e8e21991f56e8985409bc742f2501440
Mistress Plague <36628963+MistressPlague@users.noreply.github.com>
2022-11-16 01:58:36 +0000

    Update WorldTags.cs

--- WorldTags.cs
+++ WorldTags.cs
@@ -12,7 +12,7 @@ public class WorldTags : UdonSharpBehaviour
     private string[] displayNamesToApplyTo;
 	
     [SerializeField]
-    private int HeightMultiplier = 0;
+    private float HeightMultiplier = 0;
 
     private GameObject InstantiatedObj;
 
